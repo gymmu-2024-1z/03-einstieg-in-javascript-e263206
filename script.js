134cad3833e622fac2de841e3d1ef4a2b7fd59f4
Marlon Ernst <e263206@edu.sbl.ch>
2024-10-25 06:12:33 +0000

    Aufgabe 8 abgeschlossen

--- script.js
+++ script.js
@@ -89,9 +89,9 @@ export function aufgabe08(args) {
     const currentElement = input[i]
 
     if (currentElement === "e") {
-    } else if (currentElement === "E") {
+      result.push(3)
     } else {
-      result.push()
+      result.push(currentElement)
     }
   }
   return result.join("")
